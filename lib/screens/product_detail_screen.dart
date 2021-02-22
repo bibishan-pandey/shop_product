08f71627b5414d9f93432e2a71318204dff93937
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-02-22 19:17:50 +0545

    feat: adding contents in product detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -18,8 +18,90 @@ class ProductDetailScreen extends StatelessWidget {
         title: Text(product.title),
         centerTitle: false,
       ),
-      body: Center(
-        child: Text('Product $id'),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 500,
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Image.network(
+                      product.imageUrl,
+                      height: 300,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                      loadingBuilder: (context, child, loadingProgress) {
+                        return loadingProgress == null
+                            ? child
+                            : LinearProgressIndicator();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 16,
+                  ),
+                  width: (MediaQuery.of(context).size.width -
+                          MediaQuery.of(context).viewInsets.left -
+                          MediaQuery.of(context).viewInsets.right) *
+                      0.4,
+                  child: Text(
+                    product.title,
+                    style: TextStyle(
+                        fontSize: 23,
+                        color: Theme.of(context).textTheme.headline1.color),
+                    overflow: TextOverflow.fade,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 16,
+                  ),
+                  child: Chip(
+                    label: Text(
+                      product.price.toStringAsFixed(2),
+                      style: TextStyle(
+                        fontSize: 23,
+                        color: Colors.white,
+                      ),
+                    ),
+                    avatar: Icon(
+                      Icons.attach_money,
+                      color: Colors.white,
+                    ),
+                    elevation: 5,
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ],
+            ),
+            Divider(),
+            Container(
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
+              ),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
