7849a85ccc5b765871ffc6739848399c2eeeac7c
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-02-22 15:08:09 +0545

    feat: configuring multiple providers

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_product/config/themes/dark_theme.dart';
 import 'package:shop_product/config/themes/light_theme.dart';
+import 'package:shop_product/providers/cart_provider.dart';
 import 'package:shop_product/providers/product_provider.dart';
 import 'package:shop_product/screens/product_detail_screen.dart';
 import 'package:shop_product/screens/products_overview_screen.dart';
@@ -14,25 +15,34 @@ void main() {
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
   ]);
-  runApp(MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (ctx) => ProductProvider(),
+      ),
+      ChangeNotifierProvider(
+        create: (ctx) => CartProvider(),
+      ),
+    ],
+    child: App(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
+  static const title = 'Shop Product';
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => ProductProvider(),
-      child: MaterialApp(
-        title: 'Shop Product',
-        theme: lightTheme,
-        darkTheme: darkTheme,
-        themeMode: ThemeMode.system,
-        home: ProductOverviewScreen(),
-        routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-        },
-        debugShowCheckedModeBanner: false,
-      ),
+    return MaterialApp(
+      title: title,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.system,
+      home: ProductOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+      },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
